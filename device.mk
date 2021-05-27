94a32f3041ce0539a3e2dfbe2c27ec4c61c512ec
A2L5E0X1 <alexed25@protonmail.com>
2021-05-27 17:13:50 +0200

    warsaw: import initial proprietary-files
    Change-Id: If487fffba7fd682732a38c6705075a76349832fa

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ DEVICE_PATH := device/huawei/warsaw
 ## Inherit common device
 $(call inherit-product, device/huawei/hi6250-8-common/hi6250.mk)
 
+## Inherit vendor blobs
+$(call inherit-product, vendor/huawei/warsaw/warsaw-vendor.mk)
+
 # Display
 TARGET_SCREEN_DENSITY := 480
 TARGET_SCREEN_HEIGHT := 1920
